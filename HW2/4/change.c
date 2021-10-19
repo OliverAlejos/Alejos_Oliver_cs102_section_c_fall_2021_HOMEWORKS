967690895b31825cad355c03b0f7afca86f53906
OliverAlejos <90672319+OliverAlejos@users.noreply.github.com>
2021-10-18 23:18:46 -0400

    Update change.c

--- HW2/4/change.c
+++ HW2/4/change.c
@@ -1,1 +1,33 @@
+/*
+4 - (2 pts) Given an amount of US money in cents, compute the number of quarters, dimes, nickels, 
+and pennies needed to minimize the number of coins.
+*/
 
+#include <stdio.h>
+
+int main()
+{
+    int m, q, d, n, p;
+    
+    printf("Give me some cents \n");
+    scanf("%i", &m);
+    
+    if (m % 25 == 0)
+        {
+        q = m/25;
+        printf("%i quarter(s)", q);  
+        }
+    else {
+        if (m % 10 ==0)
+            {
+            d = m/10;
+            printf("%i dimes", d);
+            }
+            else  if (m % 5 ==0){
+            n = m/5;
+            printf("%i nickels", n);}
+                else {
+                printf("%i pennies", m);}
+    }
+    return 0;
+}
