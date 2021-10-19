a3c5cfc8a27061b8d18c93ba0d65a4e4319b2419
OliverAlejos <90672319+OliverAlejos@users.noreply.github.com>
2021-10-18 20:22:24 -0400

    Update Perimeter.c

--- HW2/1/Perimeter.c
+++ HW2/1/Perimeter.c
@@ -1,1 +1,22 @@
+/*
+1 - (1 pt) Write a program that prints the perimeter of a rectangle given its height and width. 
+Ask the user to input the two dimensions. Do you remember the formula for perimeter of a rectangle
+*/
 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define Success 0
+int main(void)
+{
+    int l, w;
+    int Perimeter;
+
+    printf("INPUT LENGTH AND WIDTH: \n");
+    scanf("%i %i", &l, &w);
+    Perimeter = l+l+w+w;
+    printf("THE PERIMETER IS: %i", Perimeter);
+    
+    return 0;
+}
